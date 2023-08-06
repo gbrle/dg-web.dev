96981df7811010f647aed671b7795a8bf96a0648
David Gabriele <david.gabriele@outlook.fr>
2023-08-06 16:11:44 +0200

    Installation tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,7 @@ Encore
         config.useBuiltIns = 'usage';
         config.corejs = '3.23';
     })
+    .enablePostCssLoader()
 
     // enables Sass/SCSS support
     //.enableSassLoader()
