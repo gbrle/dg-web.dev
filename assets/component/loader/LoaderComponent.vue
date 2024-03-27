738bf420c69df082269507facd647b16abcbf00c
David Gabriele <dgabriele@com-company.fr>
2024-03-27 15:34:17 +0100

    Creation composant Loader

---
+++ assets/component/loader/LoaderComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="loader-container">
+        <div class="loader"></div>
+    </div>
+</template>
+
+<style scoped>
+.loader-container {
+    position: fixed;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 100;
+}
+
+.loader {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    display: inline-block;
+    border-top: 4px solid #1853e5;
+    border-right: 4px solid transparent;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+}
+
+.loader::after {
+    content: '';
+    box-sizing: border-box;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    border-left: 4px solid #ff3d00;
+    border-bottom: 4px solid transparent;
+    animation: rotation 0.5s linear infinite reverse;
+}
+
+@keyframes rotation {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
---
+++ assets/component/loader/LoaderContainerComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import LoaderComponent from '@/component/loader/LoaderComponent.vue'
+
+defineProps({
+    loading: {
+        type: Boolean,
+        default: true
+    }
+})
+</script>
+
+<template>
+    <LoaderComponent v-if="loading" />
+    <slot v-else></slot>
+</template>
